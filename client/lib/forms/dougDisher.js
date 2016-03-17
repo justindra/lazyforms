6fd1b03c36580016b7f7b1b58984e400d0bd77a9
Justin Indra Rahardjo <Justin Indra Rahardjo>
2016-03-17 21:37:32 +0700

    Added more doug disher template

--- client/lib/forms/dougDisher.js
+++ client/lib/forms/dougDisher.js
@@ -5,7 +5,7 @@ LazyForms['generatePdfDougDisher'] = function (userId) {
       doc = new jsPDF();
 
   doc.setFontSize(11);
-  doc.addImage(REForms.toowong_DougDisher[0], 'JPEG', 0, 0, 210, 297);
+  doc.addImage(REForms.toowong_DougDisher.forms[0], 'JPEG', 0, 0, 210, 297);
 
   // Personal details
   if (profile.first_name) doc.text(10, 146, profile.first_name);
@@ -20,17 +20,27 @@ LazyForms['generatePdfDougDisher'] = function (userId) {
   if (profile.phone.work) doc.text(74, 173, profile.phone.work);
   if (profile.phone.mobile) doc.text(136, 173, profile.phone.mobile);
   if (profile.dob) doc.text(10, 189, moment(profile.dob).format('DD/MM/YYYY'));
-  doc.text(76, 189, profile.identifications.drivers_license.number);
-  doc.text(134, 189, profile.identifications.drivers_license.state);
-  doc.text(10, 198, profile.identifications.passport.number + ' / '
-                  + moment(profile.identifications.passport.expiry).format('DD/MM/YYYY'));
-  doc.text(76, 198, profile.identifications.passport.country);
-  doc.text(134, 207, profile.identifications.car.type + ' / '
-                  + profile.identifications.car.plate);
+  if (profile.identifications.drivers_license) {
+    doc.text(76, 189, profile.identifications.drivers_license.number);
+    doc.text(134, 189, profile.identifications.drivers_license.state);
+  }
+  if (profile.identifications.passport) {
+    doc.text(10, 198, profile.identifications.passport.number + ' / '
+                    + moment(profile.identifications.passport.expiry).format('DD/MM/YYYY'));
+    doc.text(76, 198, profile.identifications.passport.country);
+  }
+  if (profile.identifications.car) doc.text(134, 207, profile.identifications.car.type + ' / '
+                                                    + profile.identifications.car.plate);
+
+  doc.addPage();
+  doc.addImage(REForms.toowong_DougDisher.forms[1], 'JPEG', 0, 0, 210, 297);
+
+  doc.addPage();
+  doc.addImage(REForms.toowong_DougDisher.forms[2], 'JPEG', 0, 0, 210, 297);
 
 
-  var splitTitle = doc.splitTextToSize("Hello what is this crazy title doing here. its so long and windy and is it gonna wrap? I don't know its just so crazy", 50);
-  doc.text(15, 20, splitTitle);
+  // var splitTitle = doc.splitTextToSize("Hello what is this crazy title doing here. its so long and windy and is it gonna wrap? I don't know its just so crazy", 50);
+  // doc.text(15, 20, splitTitle);
 
   doc.save('toowongDougDisher-' + profile.name + '.pdf');
 }
