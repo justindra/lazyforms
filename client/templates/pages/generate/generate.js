6b023c365a8d077c2cfcc16e6e4aa49495e32f95
Justin Indra Rahardjo <Justin Indra Rahardjo>
2016-03-21 19:20:29 +0700

    Added text and terms page

--- client/templates/pages/generate/generate.js
+++ client/templates/pages/generate/generate.js
@@ -18,6 +18,7 @@ Template.generate.events({
       }
     }
 
+    Meteor.call('saveProperty', evt.target.user_id.value, rental_details);
     LazyForms['generatePdf' + evt.target.agent.value](evt.target.user_id.value, rental_details);
   },
   'submit #location-rental-form, submit #pet-rental-form': function (evt, template) {
