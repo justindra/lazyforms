6e80991d979b0f6cd50686210786ef080ab0fdc9
Justin Indra Rahardjo <Justin Indra Rahardjo>
2016-03-21 18:24:06 +0700

    Added generate forms page

--- client/lib/forms/dougDisher.js
+++ client/lib/forms/dougDisher.js
@@ -1,11 +1,21 @@
 if (typeof LazyForms === 'undefined') { LazyForms = {}; }
 
-LazyForms['generatePdfDougDisher'] = function (userId) {
+LazyForms['generatePdfdougDisher_toowong'] = function (userId, rentalDetails) {
   var profile = Meteor.users.findOne(userId).profile,
       doc = new jsPDF();
 
   doc.setFontSize(11);
   doc.addImage(REForms.toowong_DougDisher.forms[0], 'JPEG', 0, 0, 210, 297);
+
+  // Rental Details
+  if (rentalDetails.address) doc.text(10, 88, rentalDetails.address);
+  if (rentalDetails.weekly_rent) doc.text(10, 104, rentalDetails.weekly_rent.toString());
+  if (rentalDetails.lease_term) doc.text(90, 104, rentalDetails.lease_term.toString());
+  if (rentalDetails.lease_start) doc.text(137, 104, moment(rentalDetails.lease_start).format('DD/MM/YYYY'));
+  if (rentalDetails.num_occupants) doc.text(10, 120, rentalDetails.num_occupants);
+  if (rentalDetails.children) doc.text(108, 120, rentalDetails.children);
+  if (rentalDetails.pets.num_type) doc.text(10, 129, rentalDetails.pets.num_type);
+  if (rentalDetails.pets.registered) doc.text(108, 129, rentalDetails.pets.registered);
 
   // Personal details
   if (profile.first_name) doc.text(10, 146, profile.first_name);
@@ -32,12 +42,121 @@ LazyForms['generatePdfDougDisher'] = function (userId) {
   if (profile.identifications.car) doc.text(134, 207, profile.identifications.car.type + ' / '
                                                     + profile.identifications.car.plate);
 
+  // Current Rental Details
+  var rental_history = profile.rental_history;
+
+  if (rental_history.current) {
+    var current = rental_history.current;
+    if (current.address) doc.text(10, 222, current.address.street + ', '
+                                         + current.address.suburb + ', '
+                                         + current.address.state + ', '
+                                         + current.address.country + ' '
+                                         + current.address.postcode);
+    if (current.rent) doc.text(10, 232, current.rent.toString());
+    if (current.length) doc.text(76, 232, current.length);
+    if (current.agent.name) doc.text(10, 241, current.agent.name);
+    if (current.agent.phone) doc.text(76, 241, current.agent.phone);
+    if (current.reason) {
+      var reason = doc.splitTextToSize(current.reason, 70);
+      doc.text(134, 232, reason);
+    }
+  }
+
+  if (rental_history.previous) {
+    var previous = rental_history.previous;
+    if (previous.address) doc.text(10, 257, previous.address.street + ', '
+                                         + previous.address.suburb + ', '
+                                         + previous.address.state + ', '
+                                         + previous.address.country + ' '
+                                         + previous.address.postcode);
+    if (previous.rent) doc.text(10, 266, previous.rent.toString());
+    if (previous.length) doc.text(76, 266, previous.length);
+    if (previous.agent.name) doc.text(10, 275, previous.agent.name);
+    if (previous.agent.phone) doc.text(76, 275, previous.agent.phone);
+    if (previous.reason) {
+      var reason = doc.splitTextToSize(previous.reason, 70);
+      doc.text(134, 266, reason);
+    }
+  }
+
   doc.addPage();
   doc.addImage(REForms.toowong_DougDisher.forms[1], 'JPEG', 0, 0, 210, 297);
 
+  // Employment Details
+  var employment_history = profile.employment_history;
+  if (employment_history.current) {
+    var current = employment_history.current;
+    if (current.name) doc.text(10, 44, current.name);
+    if (current.address) doc.text (107, 44, current.address.street + ', '
+                                         + current.address.suburb + ', '
+                                         + current.address.state + ', '
+                                         + current.address.country + ' '
+                                         + current.address.postcode);
+    if (current.position) doc.text(10, 53.5, current.position);
+    if (current.manager.name) doc.text(76, 53.5, current.manager.name + ' / '
+                                               + current.manager.position);
+    if (current.manager.phone) doc.text(141, 53.5, current.manager.phone);
+    if (current.length) doc.text(10, 63, current.length);
+    if (current.income) doc.text(76, 63, current.income.toString());
+    if (current.capacity) doc.text(141, 63, current.capacity);
+  }
+
+  if (employment_history.previous) {
+    var previous = employment_history.previous;
+    if (previous.name) doc.text(10, 80, previous.name);
+    if (previous.address) doc.text (107, 80, previous.address.street + ', '
+                                         + previous.address.suburb + ', '
+                                         + previous.address.state + ', '
+                                         + previous.address.country + ' '
+                                         + previous.address.postcode);
+    if (previous.position) doc.text(10, 89, previous.position);
+    if (previous.manager.name) doc.text(76, 89, previous.manager.name + ' / '
+                                              + previous.manager.position);
+    if (previous.manager.phone) doc.text(141, 89, previous.manager.phone);
+    if (previous.length) doc.text(10, 99, previous.length);
+    if (previous.income) doc.text(76, 99, previous.income.toString());
+    if (previous.capacity) doc.text(141, 99, previous.capacity);
+  }
+
+  if (employment_history.student) {
+    var student = employment_history.student;
+    if (student.number) doc.text(10, 115, student.number);
+    if (student.institution) doc.text(76, 115, student.institution);
+    if (student.sponsorship) doc.text(141, 115, student.sponsorship.toString());
+    if (student.degree) doc.text(10, 124.5, student.degree);
+    if (student.capacity) doc.text(76, 124.5, student.capacity);
+    if (student.year) doc.text(141, 124.5, student.year.toString());
+  }
+
+  // References
+  var references = profile.references,
+      name = [[10, 141], [10, 150.5], [10, 160]],
+      relationship = [[76, 141], [76, 150.5], [76, 160]],
+      number = [[141, 141], [141, 150.5], [141, 160]];
+  for (var i = 0; i < references.length; i++) {
+    if (references[i].name) doc.text(name[i][0], name[i][1], references[i].name);
+    if (references[i].relationship) doc.text(relationship[i][0], relationship[i][1], references[i].relationship);
+    if (references[i].number) doc.text(number[i][0], number[i][1], references[i].number);
+  }
+
+  // Emergency Contact
+  // Vertical is 176
+
+  // Payment
+  var weekly_rent = rentalDetails.weekly_rent;
+
+  if (weekly_rent) {
+    var bond = weekly_rent * 4,
+        minimum = weekly_rent * 2,
+        total = weekly_rent * 6;
+    doc.text(115, 195, weekly_rent.toString());
+    doc.text(115, 202.5, bond.toString());
+    doc.text(115, 210.5, minimum.toString());
+    doc.text(115, 218, total.toString());
+  }
+
   doc.addPage();
   doc.addImage(REForms.toowong_DougDisher.forms[2], 'JPEG', 0, 0, 210, 297);
-
 
   // var splitTitle = doc.splitTextToSize("Hello what is this crazy title doing here. its so long and windy and is it gonna wrap? I don't know its just so crazy", 50);
   // doc.text(15, 20, splitTitle);
---
+++ client/templates/pages/generate/generate.js
@@ -0,0 +1,26 @@
+Template.generate.events({
+  'submit #confirm-rental-form': function (evt, template) {
+    evt.preventDefault();
+
+    var locationForm = $('#location-rental-form')[0],
+        petForm = $('#pet-rental-form')[0];
+
+    var rental_details = {
+      address: locationForm.rental_address.value,
+      weekly_rent: parseFloat(locationForm.rental_weekly_rent.value),
+      lease_term: parseInt(locationForm.rental_lease_term.value),
+      lease_start: locationForm.rental_lease_start.value,
+      num_occupants: locationForm.rental_occupants.value,
+      children: locationForm.rental_children.value,
+      pets: {
+        num_type: petForm.rental_pets.value,
+        registered: petForm.rental_pets_registered.value
+      }
+    }
+
+    LazyForms['generatePdf' + evt.target.agent.value](evt.target.user_id.value, rental_details);
+  },
+  'submit #location-rental-form, submit #pet-rental-form': function (evt, template) {
+    evt.preventDefault();
+  }
+});
