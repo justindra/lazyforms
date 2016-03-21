6385d59ff4c6e514f2375a11eb753f1a0e8eac9c
Justin Indra Rahardjo <Justin Indra Rahardjo>
2016-03-21 22:18:01 +0700

    Added google ads

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -1,3 +1,7 @@
 Template.registerHelper('formatDateForInput', function (value) {
   return moment(value).format('YYYY-MM-DD');
 });
+
+Template.registerHelper('canRunAds', function () {
+  return (typeof canRunAds === 'undefined') ? false : canRunAds;
+});
---
+++ client/templates/pages/extras/extras.js
@@ -0,0 +1,5 @@
+Template.googleAds.helpers({
+  unique: function () {
+    return (new Date()).valueOf();
+  }
+});
